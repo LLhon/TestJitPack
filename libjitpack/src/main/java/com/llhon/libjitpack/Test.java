06da70107797c94dc595e5e45c269b1ca407e732
LLhon <hon0608@163.com>
2020-07-31 17:20:34 +0800

    v1.0.2

---
+++ libjitpack/src/main/java/com/llhon/libjitpack/Test.java
@@ -0,0 +1,11 @@
+package com.llhon.libjitpack;
+
+/**
+ * Created by LLhon
+ */
+public class Test {
+
+    public static void print(String msg){
+        System.out.println("print:" + msg);
+    }
+}
